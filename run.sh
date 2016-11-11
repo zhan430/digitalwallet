cabd902a87eace3169a3304e65b8dcf1a06a3131
zhan430 <zhan430@usc.edu>
2016-11-10 18:42:51 -0800

    csv to txt

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python ./src/source.py ./paymo_input/batch_payment.csv ./paymo_input/stream_payment.csv ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
+python ./src/source.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
